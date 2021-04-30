9f48bd47e4fd602a382f048d2a651aa81d870246
Faith SODIPE <faithsodipe@gmail.com>
2021-04-30 12:01:46 +0100

    pulled from master remote

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:haggle_clone/screens/main.dart';
+import 'package:haggle_clone/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
